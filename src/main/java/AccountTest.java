a53eb340ce4b63f3bf330a789781e4d64967d1d3
JazminRondon <Jazmin_Rondon@outlook.com>
2022-09-21 21:57:17 -0400

    account

---
+++ src/main/java/AccountTest.java
@@ -0,0 +1,7 @@
+public class AccountTest {
+    public static void main(String[] args) {
+    Account account=new Account();
+    account.setBalance(130.0);
+        System.out.println(account.getBalance());
+    }
+}
